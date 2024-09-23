9eebf01d7c069c2fb1a216dc9472554996a75490
Ashkeyev Ali <alias@halykbank.kz>
2024-09-23 22:21:07 +0500

    Управление размерами окна для desktop в common коде

--- common/core-compose/src/commonMain/kotlin/desktop/AppSettings.kt
+++ common/core-compose/src/commonMain/kotlin/desktop/AppSettings.kt
@@ -1,4 +1,18 @@
 package desktop
 
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.flow.MutableStateFlow
+
 class AppSettings {
+    private val _windowState: MutableStateFlow<WindowState> = MutableStateFlow(WindowState.Custom(1024.dp, 800.dp))
+    val windowState: MutableStateFlow<WindowState> = _windowState
+
+    fun updateWindowState(windowState: WindowState) {
+        _windowState.value = windowState
+    }
+}
+
+val LocalAppSettings = staticCompositionLocalOf<AppSettings> {
+    error("No AppSettings provided")
 }
